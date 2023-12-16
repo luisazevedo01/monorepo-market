64d806737a5eee2f5d538fedd2200dcd3dd51af2
Luís Azevedo <luismiguelgomes2001@gmail.com>
2023-12-16 22:52:52 -0100

    ADD: Material UI and AddProductModal

---
+++ client/src/components/modal/AddProductModal.tsx
@@ -0,0 +1,54 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, TextField } from "@mui/material"
+
+interface ModalProps {
+    open: boolean;
+    handleClose: () => void;
+}
+
+function AddProductModal({ open, handleClose }: ModalProps) {
+    return (
+        <Dialog open={open} onClose={handleClose}>
+            <DialogTitle className="text-center">Adicionar Produto</DialogTitle>
+            <DialogContent>
+                <DialogContentText className="text-center" padding={1}>
+                    Adiciona um produto que tenhas em stock para que outras pessoas o possam comprar!
+                </DialogContentText>
+                <Grid container direction="column" padding="0 5rem">
+                    <TextField
+
+                        autoFocus
+                        id="title"
+                        margin="normal"
+                        label="Title"
+                        type="text"
+                        variant="standard"
+                    />
+                    <TextField
+
+                        autoFocus
+                        id="title"
+                        margin="normal"
+                        label="Price"
+                        type="text"
+                        variant="standard"
+                    />
+                    <TextField
+
+                        autoFocus
+                        id="title"
+                        margin="normal"
+                        label="Owner"
+                        type="text"
+                        variant="standard"
+                    />
+                </Grid>
+            </DialogContent>
+            <DialogActions>
+                <Button color="success" variant="outlined" onClick={handleClose}>Cancelar</Button>
+                <Button color="success" variant="outlined" onClick={handleClose}>Adicionar</Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
+
+export default AddProductModal
