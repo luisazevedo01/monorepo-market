6b82f7df3a400c5951ebd1cf2e8e0581445e9695
Luís Azevedo <luismiguelgomes2001@gmail.com>
2023-12-19 22:41:22 -0100

    fix: removed unused code and condition to render mocks instead of api data

--- client/src/app/market/page.tsx
+++ client/src/app/market/page.tsx
@@ -1,23 +1,21 @@
 'use client'
 import { PRODUCTS } from "@client/src/graphql/queries/product";
 import { ProductCard } from "../../components/cards/ProductCard";
-import { MOCKED_PRODUCTS } from "../../constants/mocked-products";
 import { useQuery } from "@apollo/client";
-import { useEffect, useMemo, useState } from "react";
-import { TestCard } from "@client/src/components/cards/TestCard";
-import Modal from "@client/src/components/modal/AddProductModal";
+import { useMemo, useState } from "react";
 import { Button, Grid } from "@mui/material";
 import AddProductModal from "@client/src/components/modal/AddProductModal";
 import Search from "@client/src/components/inputs/search";
 
 export default function Market() {
     const [isOpen, setIsOpen] = useState(false);
-    const [filteredProducts, setFilteredProducts] = useState();
     const { loading, data } = useQuery(PRODUCTS, { variables: { ownerId: 1 } });
-    
+
     const products = useMemo(() => {
         return data?.products;
     }, [data])
+
+    const [filteredProducts, setFilteredProducts] = useState<[] | undefined>(products);
 
     const addProduct = () => {
         setIsOpen(true)
@@ -30,7 +28,7 @@ export default function Market() {
             <div className="flex justify-center py-5">
                 <Button onClick={addProduct} variant="contained" color="success">Adicionar Produto</Button>
             </div>
-            <Search products={products} setProduct={setFilteredProducts}/>
+            <Search products={products} setProduct={setFilteredProducts} />
             <Grid
                 container
                 minHeight="100vh"
@@ -39,7 +37,7 @@ export default function Market() {
                 wrap="wrap"
                 padding={20}
             >
-                {(filteredProducts ? filteredProducts : products).map((product: any, idx: any) => (
+                {filteredProducts?.map((product: any, idx: any) => (
                     <ProductCard key={`product_${idx}`} img={product.image} title={product.title} price={product.price} />
                 ))}
             </Grid>
